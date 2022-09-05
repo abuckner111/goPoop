84ed64f6257e6106b665be590e55589aaaedbc0d
abuckner111 <74509645+abuckner111@users.noreply.github.com>
2022-09-04 21:32:16 -0400

    Added Input System and game control logic

--- Go Poop/Assets/Scripts/Control/gameController.cs
+++ Go Poop/Assets/Scripts/Control/gameController.cs
@@ -1,18 +1,81 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.InputSystem;
+
 
 public class gameController : MonoBehaviour
 {
+
+    public GameObject   pauseWindow;
+    public bool         paused;
+    private bool        pausedD;
+    public float        defaultTimeSpeed = 1f;
+
+    private InputActions _input;
+
+    private void Pause()
+    {
+        Time.timeScale = 0f;
+    }
+
+    private void UnPause()
+    {
+        Time.timeScale = defaultTimeSpeed;
+    }
+
+    private void PauseInput(InputAction.CallbackContext ctx)
+    {
+        pauseWindow.SetActive(true);
+    }
+
+    private void ControlPause()
+    {
+        if(paused != pausedD)
+        {
+            pausedD = paused;
+            if(paused)
+            {
+                Pause();
+            }
+            else
+            {
+                UnPause();
+            }
+        }
+    }
+
+
+    private void OnEnable()
+    {
+        _input = new InputActions();
+        _input.menu.Enable();
+
+        _input.menu.Pause.started += PauseInput;
+    }
+
+    private void OnDisable()
+    {
+        _input.menu.Pause.started -= PauseInput;
+
+        _input.menu.Disable();
+    }
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        paused = pauseWindow.activeSelf;
+        pausedD = !paused;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if(pauseWindow.activeSelf != paused)
+        {
+            paused = pauseWindow.activeSelf;
+        }
+
+        ControlPause();
     }
 }
